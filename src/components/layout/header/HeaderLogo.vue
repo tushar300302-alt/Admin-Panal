7179981703ac05a8f5ec55e5db5fa10782debe94
Aditya Bagade <adityabagade04@gmail.com>
2025-11-06 18:56:21 +0530

    update the web

---
+++ src/components/layout/header/HeaderLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <router-link to="/" class="lg:hidden">
+  </router-link>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/tailadmin-vuejs/' : '/',
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+})
